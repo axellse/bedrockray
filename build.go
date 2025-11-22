3d4bfacc4670cb6b22b529a51ddd7e1bc258f524
axell <mail@axell.me>
2025-11-22 22:29:08 +0100

    first commit

---
+++ build.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func main() {
+	fmt.Println("Started bedrockray!")
+	mcdir := os.Getenv("MCDIR")
+	mcid := os.Getenv("MCID")
+
+	if mcdir == "" {
+		fmt.Println("FATAL: no mcdir specified")
+		os.Exit(1)
+	}
+	if mcid == "" {
+		fmt.Println("FATAL: no mcid specified")
+		os.Exit(1)
+	}
+
+	if _, err := os.Stat(mcdir); errors.Is(err, os.ErrNotExist) {
+  		fmt.Println(mcdir, "does not exist, creating...")
+		err := os.MkdirAll(mcdir, 0755)
+		if err != nil {
+			fmt.Println("FATAL: failed creating mcdir:", err)
+			os.Exit(1)
+		}
+	}
+
+	fmt.Println("mcdir alright, now checking for server", mcid)
+	serverPath := filepath.Join(mcdir, "bdray-server-" + mcid)
+	if _, err := os.Stat(serverPath); errors.Is(err, os.ErrNotExist) {
+		fmt.Println("server does not exist!")
+		fmt.Println("preparing to create server", mcid)
+		os.Mkdir(serverPath, 0755)
+
+		mcserverdl := os.Getenv("MCSERVERDL")
+		if mcserverdl == "" {
+			mcserverdl = "https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-1.21.124.2.zip"
+		}
+		fmt.Println("now pulling server from", mcserverdl)
+
+		clean := func ()  {
+			err := os.Remove(serverPath)
+			if err != nil {
+				fmt.Println("ion ever care anymore this servers completly fucked💀🥀")
+			}
+			os.Exit(1)
+		}
+
+		resp, err := http.Get(mcserverdl)
+		if err != nil {
+			fmt.Println("FATAL: could not start server download!")
+			clean()
+		}
+
+		serverZipPath := filepath.Join(serverPath, "server.zip")
+		serverZip, err := os.Open(serverZipPath)
+		if err != nil {
+			fmt.Println("FATAL: could not open server zip!")
+			clean()
+		}
+
+		_, err = io.Copy(serverZip, resp.Body)
+		if err != nil {
+			fmt.Println("FATAL: could not unzip server!")
+			clean()
+		}
+		cmd := exec.Command("tar", "-xf", "server.zip") //tar is also on windows
+		cmd.Path = serverPath
+
+		err = cmd.Run()
+		if err !=  nil {
+			fmt.Println("FATAL: failed unzipping server")
+			clean()
+		}
+
+		err = os.Remove(serverZipPath)
+		if err !=  nil {
+			fmt.Println("failed removing server zip file, fine!")
+		}
+	}
+
+	fmt.Println("pulled server ok, now writing server.properties")
+	ba, err := os.ReadFile("./server.properties")
+	if err != nil {
+		fmt.Println("FATAL: could not read server.properties")
+	}
+
+	err = os.WriteFile(filepath.Join(serverPath, "server.properties"), ba, 0775)
+	if err != nil {
+		fmt.Println("FATAL: could not write server.properties")
+	}
+
+	fmt.Println("server alright, now starting server.")
+	serverBinPath := filepath.Join(mcdir, "bedrock-server")
+
+	cmd := exec.Command(serverBinPath)
+	err = cmd.Start()
+	if err != nil {
+		fmt.Println("FATAL: server exited.")
+		os.Exit(1)
+	}
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# bedrockray
+run a bedrock minecraft server with ray
+## how to use?
+it is recommnded to fork the repo so you can edit the config.
+
+specify a unique identifer for the server via the enviroument variable `MCID` and a place to put the server with `MCDIR`. since ray router is http only, dont specify a domain.
+
+you can specify where to download the server from with `MCSERVERDL`
